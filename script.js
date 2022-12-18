782923000396e63b66a05e4d5d26a2a5431a36b1
peterk <peterkjx@gmail.com>
2022-12-18 14:24:52 +0800

    18dec

---
+++ script.js
@@ -0,0 +1,29 @@
+// Object.prototype.byIndex = function(index) {
+//   var keys = Object.keys(this);
+//   return this[keys[index]];
+// };
+
+const arrNames = ["peterKuang", "weiLong", "joel", "daphne"];
+
+for (let i = 0; i<arrNames.length; i++) {
+  let arrNames[i] = [""];
+}
+
+console.log(peterKuang);
+
+// const allNames = {
+//   peterKuang: {
+//     hasMet: ["peterKuang", "weiLong"]
+//   },
+//   weiLong: {
+//     subGroupedpeterKuang: false,
+//     subGroupedjoel: false,
+//     subGroupeddaphne: false,
+//   },
+// } 
+
+// allNames.peterKuang.hasMet.push(arrNames[3]);
+
+
+console.log(allNames.peterKuang.hasMet);
+console.log(arrNames);
