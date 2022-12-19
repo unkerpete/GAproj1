0de92f8d489806f9f88e44d30800940231403f2d
peterk <peterkjx@gmail.com>
2022-12-19 14:11:04 +0800

    Delete name button functionality complete. Generate Subroup button created.

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-// Object.prototype.byIndex = function(index) {
-//   var keys = Object.keys(this);
-//   return this[keys[index]];
-// };
+// elements selectors
+const addBtn = document.querySelector("#addToDisplay");
+const inputValue = document.querySelector("#namesInput");
+const displayTBody = document.querySelector("tbody");
+const displaySection = document.querySelector(".displaySection");
+const generateBtn = document.querySelector("#generateBtn");
 
-const arrNames = ["peterKuang", "weiLong", "joel", "daphne"];
+// event listeners
+addBtn.addEventListener("click", addToDisplay);
+generateBtn.addEventListener("click", generateSubGroupsClicked);
 
+// display input names and corresponding delete button to display section
+function addToDisplay(e) {
+  e.preventDefault();
+  const newName = inputValue.value;
+  if (newName !== "") {
+    // TO ADD: reject numbers
+    const newTr = document.createElement("tr");
 
-console.log(peterKuang);
+    // creates the new name and corresponding button
+    newTr.innerHTML = `<td class="finalNames" id="${newName}">${newName}</td><td><button class="btn btn-danger btnDelete" id="${newName}del">Delete</button></td>`;
 
-// const allNames = {
-//   peterKuang: {
-//     hasMet: ["peterKuang", "weiLong"]
-//   },
-//   weiLong: {
-//     subGroupedpeterKuang: false,
-//     subGroupedjoel: false,
-//     subGroupeddaphne: false,
-//   },
-// } 
+    // appends the new names unto the display
+    displayTBody.append(newTr);
 
-// allNames.peterKuang.hasMet.push(arrNames[3]);
+    // resets the input field to empty
+    inputValue.value = "";
 
+    // make the delete buttons delete itself and name
+    document.querySelector(`#${newName}del`).addEventListener("click", (e) => {
+      document.querySelector(`#${newName}`).parentElement.remove();
+    });
+  }
+}
 
-// console.log(allNames.peterKuang.hasMet);
-// console.log(arrNames);
+// array container to store submitted names
+const allNames = [];
+
+// variable to store number of members per sub-group
+let subGroupSize = "";
+
+// Generate Sub-Groups button function and push all names into an array
+function generateSubGroupsClicked() {
+  // variable to store number of members per sub-group
+  subGroupSize = document.querySelector("#subGroupSize").value;
+  another();
+}
+
+function another() {
+  console.log(subGroupSize);
+}
