2eafaa61a5c065ef1fa38db3b7dfb0de85623072
peterk <peterkjx@gmail.com>
2022-12-20 15:13:55 +0800

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const addBtn = document.querySelector("#addToDisplay");
 const inputValue = document.querySelector("#namesInput");
 const displayTBody = document.querySelector("tbody");
-const displaySection = document.querySelector(".displaySection");
+const displaySection = document.querySelector(".bottomDisplaySection");
 const generateBtn = document.querySelector("#generateBtn");
 
 // event listeners
@@ -39,31 +39,35 @@ const allSubmittedNames = [];
 // variable to store amount of members of the overall group
 let overallGroupSize = 0;
 
-// variable to store amount of members per sub-group
-let subGroupSize = 0;
-
 // variable to keep track of how many sessions created
 let sessionCounter = 1;
 
+// variable to store number of groups required
+let columnsRequired = 0;
+
 // Generate Sub-Groups button function and push all names into an array
 function generateSubGroupsClicked() {
-  // variable to store number of members per sub-group
-  subGroupSize = document.querySelector("#subGroupSize").value;
   // code to store each name (as an object) into an array
   const arrOfNames = document.querySelectorAll(".finalNames");
+  // variable to store number of members per sub-group
+  const subGroupSize = document.querySelector("#subGroupSize").value;
   for (let i = 0; i < arrOfNames.length; i++) {
     allSubmittedNames[i] = {
       name: arrOfNames[i].innerText,
-      hasMet: []
-    }
+      hasMet: [],
+    };
   }
   // variable to store amount of members of the overall group
   overallGroupSize = arrOfNames.length;
+
   // calls this function that will calc the subGroups(columns) required for display
-  createDisplayColumns(subGroupSize, overallGroupSize);
+  // createDisplayColumns(subGroupSize, overallGroupSize)
+
+  // store number of groups required
+  const groupsRequired = Math.floor(overallGroupSize / subGroupSize);
 
   //calls this function that will append names to each group for session1 only
-  randomiseNames();
+  randomiseNames(allSubmittedNames, subGroupSize, groupsRequired);
 
   // disable options: disable or remove button
   // removes the click event listener on generate button, effectively allowing user to only click it ONCE *****maybe remove later*****
@@ -72,11 +76,10 @@ function generateSubGroupsClicked() {
 }
 
 function createDisplayColumns(subGroupSize, overallGroupSize) {
-  const columnsRequired = Math.floor(overallGroupSize/subGroupSize);
+  columnsRequired = Math.floor(overallGroupSize / subGroupSize);
   const subGroupsDisplay = document.querySelector("#subGroupsDisplay");
-  subGroupsDisplay.innerHTML = 
-  `<div class="session text-center"><div class="row"><div class="col subGroupSession">Session ${sessionCounter}</div></div></div><button class="btn btn-success">Next Sub-Group</button>`;
-  for (let i = 1; i<columnsRequired+1; i++) {
+  subGroupsDisplay.innerHTML = `<div class="session text-center"><div class="row"><div class="col subGroupSession">Session ${sessionCounter}</div></div></div>`;
+  for (let i = 1; i < columnsRequired + 1; i++) {
     const session = document.querySelector(".subGroupSession");
     const groupNum = document.createElement("div");
     groupNum.setAttribute("class", `col s${sessionCounter}g${i}`);
@@ -86,20 +89,43 @@ function createDisplayColumns(subGroupSize, overallGroupSize) {
   sessionCounter += 1;
 }
 
-function randomiseNames() {
+function randomiseNames(arr, divider, subGroupsNum) {
   // shuffles the array of submitted names
-  let shuffledSubmittedNames = allSubmittedNames.sort(
-    () => Math.random() - 0.5
-  );
+  let shuffled = arr.sort(() => Math.random() - 0.5);
 
-  let tempArray =[];
-
-  for (let i = 0; i < shuffledSubmittedNames.length; i += subGroupSize) {
-    splitArray = shuffledSubmittedNames.slice(i, i+subGroupSize);
-    tempArray.push(splitArray);
+  // split the shuffled array into nested arrays with elements of divider no.
+  const splitNames = [];
+  for (let i = 0; i < subGroupsNum; i++) {
+    const newGroup = [];
+    for (let j = 0; j < divider; j++) {
+      let member = shuffled.pop();
+      newGroup.push(member);
+    }
+    splitNames.push(newGroup);
   }
 
-  console.log(tempArray);
-  console.log(allSubmittedNames);
-}
+  // // append the groupings and names in them
+  // for (let i = 0; i < splitNames.length; i++) {
+  //   for (let j = 0; j < splitNames[j].length; j++) {
+  //     const nameInSubgroup = document.createElement("p");
+  //     nameInSubgroup.setAttribute("class", "namesInSubGroups");
+  //     nameInSubgroup.innerText = splitNames[j].name;
+  //     const subGroup = document.querySelector(`.s${sessionCounter}g${i + 1}`);
+  //     console.log(`.s${sessionCounter}g${i + 1}`);
+  //     subGroup.append(nameInSubgroup);
+  //   }
+  // }
+  console.log(splitNames);
+  console.log(splitNames[0][0].name);
 
+  // attempt 2 of appending groupings and names
+  for (let i = 0; i < splitNames.length; i++) {
+    let namesInGroup = "";
+    for (let j = 0; j < splitNames[i][j]; j++) {
+      namesInGroup += splitNames[i][j].name;
+      displaySection.innerHTML += `<div> Group ${
+        i + 1
+      } - ${namesInGroup}</div>`;
+    }
+  }
+}
